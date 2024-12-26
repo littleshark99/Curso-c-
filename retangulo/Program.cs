db2888e68ef7aa7cb339e982b37551a4b3fce1bc
Rafael Clare <78705877+littleshark99@users.noreply.github.com>
2024-12-26 20:38:11 -0300

    Programa

---
+++ retangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Teste;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Retangulo ret = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("AREA = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+        }
+    }
+}
