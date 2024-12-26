a3c0f8f672fee4450b9c5b30fe9ec04647fd2ae5
Rafael Clare <78705877+littleshark99@users.noreply.github.com>
2024-12-26 20:53:35 -0300

    Classe e program

---
+++ Financeiro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Teste;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Funcionario func = new Funcionario();
+
+            Console.Write("Digite o nome do funcionário: ");
+            func.Nome = Console.ReadLine();
+            Console.WriteLine("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quanto paga de imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+
+
+
+        }
+    }
+}
